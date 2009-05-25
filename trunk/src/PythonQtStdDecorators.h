1578c124bca28772f82024030098a30e84583f82
florianlink <florianlink@ea8d5007-eb21-0410-b261-ccb3ea6e24a9>
2009-05-25 18:06:39 +0000

    static connects where useless, because they are already present via unbound methods

--- trunk/src/PythonQtStdDecorators.h
+++ trunk/src/PythonQtStdDecorators.h
@@ -67,11 +67,6 @@ public slots:
             PyObject* arg3 = NULL,PyObject* arg4 = NULL,PyObject* arg5 = NULL,PyObject* arg6 = NULL,PyObject* arg7 = NULL);
 #define emit
   
-  bool static_QObject_connect(QObject* sender, const QByteArray& signal, PyObject* callable) { return connect(sender, signal, callable); }
-  bool static_QObject_connect(QObject* sender, const QByteArray& signal, QObject* receiver, const QByteArray& slot) { return connect(sender, signal, receiver, slot); }
-  bool static_QObject_disconnect(QObject* sender, const QByteArray& signal, PyObject* callable) { return disconnect(sender, signal, callable); }
-  bool static_QObject_disconnect(QObject* sender, const QByteArray& signal, QObject* receiver, const QByteArray& slot) { return disconnect(sender, signal, receiver, slot); };
-  
   QObject* parent(QObject* o);
   void setParent(QObject* o, QObject* parent);
 
