d9e31e70951ddc2b6d901415124cac32441f6741
florianlink <florianlink@ea8d5007-eb21-0410-b261-ccb3ea6e24a9>
2010-02-19 21:04:26 +0000

    updated generator to LGPLed version from gitorious

--- trunk/generator/parser/rpp/pp-engine-bits.h
+++ trunk/generator/parser/rpp/pp-engine-bits.h
@@ -1,19 +1,48 @@
 /****************************************************************************
 **
-** Copyright (C) 1992-2008 Trolltech ASA. All rights reserved.
-** Copyright 2005 Roberto Raggi <roberto@kdevelop.org>
+** Copyright (C) 2008-2009 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
 **
-** This file is part of $PRODUCT$.
+** This file is part of the Qt Script Generator project on Qt Labs.
 **
-** $CPP_LICENSE$
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
 **
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
 **
 ****************************************************************************/
 
 #ifndef PP_ENGINE_BITS_H
 #define PP_ENGINE_BITS_H
+
+#include <stdio.h>
 
 namespace rpp {
 
@@ -188,6 +217,8 @@ inline pp::PP_DIRECTIVE_TYPE pp::find_directive (char const *__directive, std::s
       case 7:
         if (__directive[0] == 'i' && !strcmp (__directive, "include"))
             return PP_INCLUDE;
+        else if (__directive[0] == 'w' && !strcmp(__directive, "warning"))
+          return PP_WARNING;
         break;
 
       case 12:
