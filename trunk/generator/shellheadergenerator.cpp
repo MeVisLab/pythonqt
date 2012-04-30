d2d14a585b47e1aa4ce092e9f14c971ced217047
florianlink <florianlink@ea8d5007-eb21-0410-b261-ccb3ea6e24a9>
2012-04-30 14:17:30 +0000

    removed non-working py_hasOwner support

--- trunk/generator/shellheadergenerator.cpp
+++ trunk/generator/shellheadergenerator.cpp
@@ -265,11 +265,6 @@ void ShellHeaderGenerator::write(QTextStream &s, const AbstractMetaClass *meta_c
     s << "void delete_" << meta_class->name() << "(" << meta_class->qualifiedCppName() << "* obj) { delete obj; } ";
     s << endl;
   }
-  if (meta_class->name()=="QTreeWidgetItem") {
-    s << "bool py_hasOwner(QTreeWidgetItem* theWrappedObject) { return theWrappedObject->treeWidget()!=NULL || theWrappedObject->parent()!=NULL; }" << endl;
-  } else if (meta_class->name()=="QGraphicsItem") {
-    s << "bool py_hasOwner(QGraphicsItem* theWrappedObject) { return theWrappedObject->scene()!=NULL || theWrappedObject->parentItem()!=NULL; }" << endl;
-  }
 
   AbstractMetaFunctionList functions = getFunctionsToWrap(meta_class);
 
