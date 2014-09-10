d2494818429f98067b60bc3ea059572c7d313eed
Florian Link <5535644+florianlink@users.noreply.github.com>
2014-09-10 12:26:49 +0000

    further cleanup

--- generator/customtypes.h
+++ generator/customtypes.h
@@ -44,31 +44,4 @@
 
 #include "typesystem.h"
 
-
-class QModelIndexTypeEntry : public CustomTypeEntry
-{
-public:
-    QModelIndexTypeEntry() : CustomTypeEntry("QModelIndex")
-    {
-        setCodeGeneration(GenerateNothing);
-    }
-
-    virtual QString javaPackage() const { return "com.trolltech.qt.core"; }
-
-    virtual bool isValue() const { return true; }
-
-    virtual void generateCppJavaToQt(QTextStream &s,
-                                     const AbstractMetaType *java_type,
-                                     const QString &env_name,
-                                     const QString &qt_name,
-                                     const QString &java_name) const;
-
-    virtual void generateCppQtToJava(QTextStream &s,
-                                     const AbstractMetaType *java_type,
-                                     const QString &env_name,
-                                     const QString &qt_name,
-                                     const QString &java_name) const;
-
-};
-
 #endif
