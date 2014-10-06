80280610c27c031bd469f3e691e1466af7fb5aff
Florian Link <5535644+florianlink@users.noreply.github.com>
2014-10-06 08:51:39 +0000

    remoted QClipboard promoter

--- generated_cpp_48/com_trolltech_qt_gui/com_trolltech_qt_gui1.cpp
+++ generated_cpp_48/com_trolltech_qt_gui/com_trolltech_qt_gui1.cpp
@@ -2617,10 +2617,6 @@ void PythonQtWrapper_QClipboard::clear(QClipboard* theWrappedObject, QClipboard:
   ( theWrappedObject->clear(mode));
 }
 
-bool  PythonQtWrapper_QClipboard::event(QClipboard* theWrappedObject, QEvent*  arg__1)
-{
-  return ( ((PythonQtPublicPromoter_QClipboard*)theWrappedObject)->promoted_event(arg__1));
-}
 
 QImage  PythonQtWrapper_QClipboard::image(QClipboard* theWrappedObject, QClipboard::Mode  mode) const
 {
