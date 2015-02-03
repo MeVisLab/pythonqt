c5eea28eedd1238c0c3d4ec2edf89ad0cfadc333
florianlink <florianlink@ea8d5007-eb21-0410-b261-ccb3ea6e24a9>
2015-02-03 10:17:00 +0000

    fixed compilation

--- trunk/generator/main.h
+++ trunk/generator/main.h
@@ -72,7 +72,7 @@ struct Preprocess
         includes << QString(".");
 
 #if defined(Q_OS_WIN32)
-        char *path_splitter = ";";
+        const char *path_splitter = ";";
 #else
         const char *path_splitter = ":";
 #endif
