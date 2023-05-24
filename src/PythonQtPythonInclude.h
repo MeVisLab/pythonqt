9a794bf453dc54b15cd70a662274d3489aa6c31a
mrbean-bremen <mrbean-bremen@users.noreply.github.com>
2023-05-24 13:35:23 +0200

    Revert undefining some defines
    - this has been introduced in #89 and breaks builds including "pythread.h"

--- src/PythonQtPythonInclude.h
+++ src/PythonQtPythonInclude.h
@@ -33,19 +33,6 @@
 #ifndef __PythonQtPythonInclude_h
 #define __PythonQtPythonInclude_h
 
-// Undefine macros that Python.h defines to avoid redefinition warning.
-#ifdef _POSIX_C_SOURCE
-#  undef _POSIX_C_SOURCE
-#endif
-
-#ifdef _POSIX_THREADS
-#  undef _POSIX_THREADS
-#endif
-
-#ifdef _XOPEN_SOURCE
-#  undef _XOPEN_SOURCE
-#endif
-
 // Undefine Qt keywords that conflict with Python headers
 #ifdef slots
 #undef slots
