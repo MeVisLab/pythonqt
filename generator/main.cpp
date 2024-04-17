a9c755a2afc41684b305c86c769c5fe6e12de9e3
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-04-17 17:15:10 +0200

    Update generator/main.cpp

--- generator/main.cpp
+++ generator/main.cpp
@@ -80,7 +80,7 @@ namespace
       {
         if (!QDir(*it).exists())
         {
-          qWarning() << "Include path " << it->toUtf8() << "does not exist, ignoring it.";
+          qWarning() << "Include path " << it->toUtf8() << " does not exist, ignoring it.";
           it = includes.erase(it);
         }
         else
