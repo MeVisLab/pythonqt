616294c1cf223cc36bd4e16d8afe8ecf2c99b22c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-09-25 15:07:42 -0400

    chore(PythonQt): replace `PyInt_Check` with `PyLong_Check`
    No functional change intended.

--- src/PythonQt.cpp
+++ src/PythonQt.cpp
@@ -1656,7 +1656,7 @@ int custom_system_exit_exception_handler()
     /* If we failed to dig out the 'code' attribute,
        just let the else clause below print the error. */
   }
-  if (PyInt_Check(value))
+  if (PyLong_Check(value))
     exitcode = (int)PyInt_AsLong(value);
   else {
     PyObject *sys_stderr = PySys_GetObject(const_cast<char*>("stderr"));
